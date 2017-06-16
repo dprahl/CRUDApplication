5c0e2d52c83b5593c16d77ea31e33cf651958840
Daniel Prahl <daniel.prahl@gmail.com>
2017-06-16 13:35:41 -0400

    added restcontroller and repository

--- src/main/java/io/zipcoder/crudapp/Person.java
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -1,5 +1,7 @@
 package io.zipcoder.crudapp;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,9 +17,35 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Value("name")
     private String name;
+
+    @Value("age")
     private Integer age;
 
-    
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
 
 }
---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,48 @@
+package io.zipcoder.crudapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * Created by danielprahl on 6/16/17.
+ */
+
+@RestController
+public class PersonController {
+
+    private PersonRepository people;
+
+    @Autowired
+    public PersonController(PersonRepository people) {
+        this.people = people;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Person readPerson(@PathVariable Integer id) {
+        return this.people.findOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void deletePerson(@PathVariable Integer id){
+        people.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createPerson(String name, Integer age){
+        people.save(new Person(name, age));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public void updatePerson(@PathVariable Integer id, String name, Integer age){
+        if(people.exists(id)){
+            // if person exists, update with new values
+            people.findOne(id).setName(name);
+            people.findOne(id).setAge(age);
+        }else{
+            // if person does not exist, create one
+            people.save(new Person(name, age));
+        }
+    }
+
+}
